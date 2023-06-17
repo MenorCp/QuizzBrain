27b49217efc5042c5fe29d053f5696dc7b2f483e
Cauã Mendes <cauamgomes6@gmail.com>
2023-06-16 22:49:51 -0300

    Page entrada quizz completada

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-//[primeiro] se foi marcado [segundo] qual opção
-var selectedOption = [0, 0];
-var acertos = [0, 0];
-
-function selectOption(option ,question){
-    const selection = document.getElementById(option);
-    if(selectedOption[0] === 0 && selectedOption[1] === 0){
-        selectedOption = [1, parseInt(question)];
-        selection.className = 'option-select-on';
-    }
-    else if(parseInt(question) === selectedOption[1]){
-        selectedOption = [0, 0];
-        selection.className = 'option-select-off';
-    }
-}
-
-
-function resposta(pergunta){
-    const hits = document.getElementById('acert');
-    const buttonResp = document.getElementById('buttonResp');
-    if(pergunta === 1 && acertos[1] != pergunta){
-        if(selectedOption[1] === 3){
-            acertos[0]++;
-            acertos[1] = pergunta;
-            buttonResp.style.backgroundColor = 'green';
-        }else{
-            buttonResp.style.backgroundColor = 'red';
-        }
-    }
-    hits.innerText = "Acertos: " + acertos[0];
+function fogueteOn(){
+    
 }
